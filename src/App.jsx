81a202254fbb1c71c55a6d7932e79a6ca65a8fca
wernnon <ruizwernnon@gmail.com>
2022-06-30 15:08:08 -0400

    finalizado

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+//App.jsx
+import './App.css'
+import './Button.jsx'
+
+function App(){
+  const palavras_chave = ["linux", 
+  "docker", "server", "rails", "douane", "my-developments","command-line", 
+  "ruby", "c-2", "linux-on-mac", "maintenance", "security", "gnome", 
+  "kubernetes","mes-developpements", "ubuntu", "python", "vcs", "capybara", 
+  "contribution", "git", "apt","bazaar", "chef", "cucumber", "debian", "game", 
+  "howto", "packaging", "testing", "boost", "debug","devise", "elixir", "gtk", 
+  "homebrew", "information", "ssh", "tuxtremsplit", "xpath"]
+  const lista = palavras_chave.map(
+    (c)=> 
+    <p className='box'>{c}</p>
+  )
+return(
+  <div className='content'>
+     
+      {lista}
+  
+  </div>
+
+)
+}
+export default App
